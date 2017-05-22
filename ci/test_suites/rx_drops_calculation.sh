bd6868c5a7cda8f6035f27c3acd27b30ac127703
sgudz <vortexuelpm@gmail.com>
2017-05-22 11:44:46 +0300

    Update rx_drops_calculation.sh

--- ci/test_suites/rx_drops_calculation.sh
+++ ci/test_suites/rx_drops_calculation.sh
@@ -17,9 +17,9 @@ ifconfig $IFACE > /tmp/$IFACE.data_before_test
 RX_DROPPED_BEFORE=`cat /tmp/$IFACE.data_before_test | grep "RX packets" | grep -Eo "dropped:[0-9]*" | cut -d ":" -f2`
 RX_TOTAL_BEFORE=`cat /tmp/$IFACE.data_before_test | grep "RX packets" | grep -Eo "packets:[0-9]*" | cut -d ":" -f2`
 
-echo "RX_TOTAL BEFORE TEST is $RX_TOTAL_BEFORE"
-echo "RX_DROPPED_BEFORE TEST is $RX_DROPPED_BEFORE"
-
+#echo "RX_TOTAL BEFORE TEST is $RX_TOTAL_BEFORE"
+#echo "RX_DROPPED_BEFORE TEST is $RX_DROPPED_BEFORE"
+echo "Waiting for $TIME second to collect data"
 sleep $TIME
 
 ifconfig $IFACE > /tmp/$IFACE.data_after_test
@@ -27,13 +27,15 @@ ifconfig $IFACE > /tmp/$IFACE.data_after_test
 RX_DROPPED_AFTER=`cat /tmp/$IFACE.data_after_test | grep "RX packets" | grep -Eo "dropped:[0-9]*" | cut -d ":" -f2`
 RX_TOTAL_AFTER=`cat /tmp/$IFACE.data_after_test | grep "RX packets" | grep -Eo "packets:[0-9]*" | cut -d ":" -f2`
 
-echo "RX_TOTAL AFTER TEST is $RX_TOTAL_AFTER"
-echo "RX_DROPPED_AFTER TEST is $RX_DROPPED_AFTER"
+#echo "RX_TOTAL AFTER TEST is $RX_TOTAL_AFTER"
+#echo "RX_DROPPED_AFTER TEST is $RX_DROPPED_AFTER"
 
 let RX_DURING_TEST=$RX_TOTAL_AFTER-$RX_TOTAL_BEFORE
 let RX_PER_SEC=$RX_DURING_TEST/$TIME
-echo "RX PER SEC is $RX_PER_SEC"
+echo "RX packets per second: $RX_PER_SEC pps"
 
 let DROP_DURING_TEST=$RX_DROPPED_AFTER-$RX_DROPPED_BEFORE
 let PERCENTAGE=$DROP_DURING_TEST*100/$RX_DURING_TEST
-echo "$PERCENTAGE %"
+echo "Total packets recieved during test: $RX_DURING_TEST"
+echo "Total dropped packets during test: $DROP_DURING_TEST"
+echo "$PERCENTAGE % dropped packets"
