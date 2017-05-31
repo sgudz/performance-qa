050e55f678ef8610e23f1bc436461f42834e603a
sgudz <vortexuelpm@gmail.com>
2017-05-31 18:37:44 +0300

    Create dpdk_pktgen.sh

---
+++ ci/test_suites/dpdk_pktgen.sh
@@ -0,0 +1,31 @@
+apt-get install libpcap-dev make cmake git
+
+vim /etc/default/grub - default_hugepagesz=1GB hugepagesz=1G hugepages=4
+update-grub
+reboot
+
+
+wget http://fast.dpdk.org/rel/dpdk-16.11.1.tar.xz
+wget http://dpdk.org/browse/apps/pktgen-dpdk/snapshot/pktgen-dpdk-pktgen-3.1.2.tar.gz
+tar -xf dpdk-16.11.1.tar.xz
+tar -xf pktgen-dpdk-pktgen-3.1.2.tar.gz
+rm dpdk-16.11.1.tar.xz
+rm pktgen-dpdk-pktgen-3.1.2.tar.gz
+mv dpdk-stable-16.11.1/ dpdk16_11
+mv pktgen-dpdk-pktgen-3.1.2/ pktgen_312
+
+### git clone http://dpdk.org/git/dpdk
+### git clone http://dpdk.org/git/apps/pktgen-dpdk
+export RTE_SDK=/home/ubuntu/dpdk16_11
+export PKTGEN=/home/ubuntu/pktgen_312
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+cd $RTE_SDK
+make install T=x86_64-native-linuxapp-gcc
+cd $PKTGEN
+make
+modprobe uio
+insmod $RTE_SDK/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+ifdown ens4
+iface=`python3 $RTE_SDK/tools/dpdk-devbind.py -s | awk '/ens4/ {print $1}'`
+python3 $RTE_SDK/tools/dpdk-devbind.py -b igb_uio $iface
+./app/app/x86_64-native-linuxapp-gcc/pktgen -c 0x3ff -n 2 --proc-type auto --socket-mem 4096 -- -T -P -m "[2-5:6-9].0"
